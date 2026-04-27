b04c841230d58434ab206a17574a93246de0de07
youssef <y.arafa@zenithr.com>
2026-04-27 10:31:32 +0300

    docs: some changes to the folder structure

--- script.py
+++ script.py
@@ -12,5 +12,3 @@ file_path = sys.argv[1]
 with open(file_path, "rb") as f:
     files = {"file": (file_path, f, "application/pdf")}
     response = requests.post(url, files=files)
-
-print(response.json())
